3cda4b1a33a3f464f982d1bc1e81b00101c3f45d
Aaronmtz1149 <111892655+Aaronmtz1149@users.noreply.github.com>
2022-09-04 19:09:59 -0500

    Create marketing.kt

---
+++ week02/marketing.kt
@@ -0,0 +1,9 @@
+var friendsCount = 300
+        println(friendsCount)
+        if(friendsCount>500)
+        {
+            pirntln("Adopt a dog today!!")
+        }else
+        {
+            println("Buy cat food at 20%")
+        }
