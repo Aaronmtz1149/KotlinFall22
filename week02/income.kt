ad2efe4fd55252dc485e71d1866138bb0250fc4d
Aaronmtz1149 <111892655+Aaronmtz1149@users.noreply.github.com>
2022-09-06 09:45:04 -0500

    Add files via upload

---
+++ week02/income.kt
@@ -0,0 +1,7 @@
+fun main()
+{
+    var name = "Stephen Curry"
+    var age = 34
+    var annualPay = 45.78
+    println("My name is $name, my age is $age and I hope to earn $$annualPay million per year")
+}
